d10e537b39ce6c80f6e75ca351a17820aef02e0b
miguelramosalarcon <mramos20681@gmail.com>
2024-02-11 12:37:16 -0500

    update css y js

--- 33-day-news-homepage/js/script.js
+++ 33-day-news-homepage/js/script.js
@@ -2,17 +2,20 @@ const iconMenu = document.querySelector(".menu");
 const closeMenu = document.querySelector(".close");
 const menu = document.querySelector("ul");
 const menuItem = document.querySelectorAll("ul li a");
+const body = document.querySelector("body");
 
 iconMenu.addEventListener("click", () => {
   iconMenu.classList.toggle("hide");
   closeMenu.classList.toggle("hide");
   menu.classList.toggle("hide");
+  body.classList.toggle("menu-open"); // Agrega o quita la clase para oscurecer el fondo
 });
 
 closeMenu.addEventListener("click", () => {
   iconMenu.classList.toggle("hide");
   closeMenu.classList.toggle("hide");
   menu.classList.toggle("hide");
+  body.classList.toggle("menu-open"); // Agrega o quita la clase para oscurecer el fondo
 });
 
 for (let i = 0; i < menuItem.length; i++) {
@@ -20,5 +23,6 @@ for (let i = 0; i < menuItem.length; i++) {
     iconMenu.classList.toggle("hide");
     closeMenu.classList.toggle("hide");
     menu.classList.toggle("hide");
+    body.classList.toggle("menu-open"); // Agrega o quita la clase para oscurecer el fondo
   });
 }
