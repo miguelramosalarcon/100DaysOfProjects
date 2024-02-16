95e8332e1743bbcac9a981ccbee3575047b14a29
miguelramosalarcon <mramos20681@gmail.com>
2024-02-15 22:02:43 -0500

    update

---
+++ 38-day-time-tracking-dashboard/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      document.querySelector('.dashboard').classList.add('mostrar');
+    }, 500); // Ajusta este valor para controlar el retraso
+  });
