e349fe19a0720a90ee65dca5417c7e7a6c4550bb
miguelramosalarcon <mramos20681@gmail.com>
2024-02-21 22:50:04 -0500

    Update script.js

--- 45-day-coding-bootcamp-testimonials-slider/js/script.js
+++ 45-day-coding-bootcamp-testimonials-slider/js/script.js
@@ -23,3 +23,36 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "flex";
 }
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; // Ocultamos todos los slides
+  }
+  
+  slides[slideIndex - 1].style.display = "flex"; // Mostramos el slide actual
+  
+  // Aplicamos fadeIn al nuevo slide
+  var fadeIn = function(element, duration) {
+    var increment = 25 / duration;
+    var opacity = 0;
+    var interval = setInterval(function() {
+      opacity += increment;
+      element.style.opacity = opacity;
+      if (opacity >= 1) {
+        clearInterval(interval);
+      }
+    }, 25);
+  };
+  
+  fadeIn(slides[slideIndex - 1], 700); // 500ms es la duración de la animación de fadeIn
+}
