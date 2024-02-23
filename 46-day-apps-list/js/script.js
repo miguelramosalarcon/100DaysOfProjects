7e29938afe35e1828513a668f3f0f840a1468b0f
miguelramosalarcon <mramos20681@gmail.com>
2024-02-22 20:04:18 -0500

    Update script.js

--- 46-day-apps-list/js/script.js
+++ 46-day-apps-list/js/script.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
       animation: 150, 
       ghostClass: 'ghost',
       onEnd: function (evt) {
-          
           console.log('Elemento movido de', evt.oldIndex, 'a', evt.newIndex);
       }
   });
