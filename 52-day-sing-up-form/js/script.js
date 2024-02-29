01a3fa2cdbc06f3969072341bc638168f4a53f2b
miguelramosalarcon <mramos20681@gmail.com>
2024-02-28 23:07:52 -0500

    Añadiendo ojito

--- 52-day-sing-up-form/js/script.js
+++ 52-day-sing-up-form/js/script.js
@@ -97,3 +97,20 @@ function hasForbiddenCharacters(password) {
         return password.includes(char);
     });
 }
+
+
+//  funcionalidad del icono del ojo
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    var togglePasswordIcon = document.getElementById("togglePasswordIcon");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        togglePasswordIcon.classList.remove("fa-eye");
+        togglePasswordIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        togglePasswordIcon.classList.remove("fa-eye-slash");
+        togglePasswordIcon.classList.add("fa-eye");
+    }
+}
