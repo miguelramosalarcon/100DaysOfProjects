277982d3c7fb873ee5d42305a10ae86eae757311
miguelramosalarcon <mramos20681@gmail.com>
2024-04-02 20:49:33 -0500

    reto 86

---
+++ 86-day-webshop-landing-page/js/script.js
@@ -0,0 +1,34 @@
+
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownContent");
+    dropdownContent.classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn') && !event.target.matches('.arrow')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("menu-toggle");
+    const headerNav = document.getElementById("navbar");
+
+    menuToggle.addEventListener("click", function() {
+        headerNav.classList.toggle("show");
+        menuToggle.classList.toggle("fa-bars");
+        menuToggle.classList.toggle("fa-xmark");
+    });
+});
+
+
+
